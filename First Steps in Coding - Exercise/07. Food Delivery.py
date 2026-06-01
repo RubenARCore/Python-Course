62c426bc4fcf62df8053b26c8809791395a884b0
Ruben Arutyunyan <yordanov.ruben@gmail.com>
2026-06-01 21:15:17 +0300

    Create 07. Food Delivery.py

---
+++ First Steps in Coding - Exercise/07. Food Delivery.py
@@ -0,0 +1,9 @@
+chicken_menu =  float(input()) * 10.35
+fish_menu = float(input()) * 12.40
+veg_menu = float(input()) * 8.15
+
+sum = chicken_menu + fish_menu + veg_menu
+
+total_sum = sum + sum * 0.2
+
+print(total_sum + 2.5)

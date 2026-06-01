2ae0f2fbf100dcb4c2db2efd7775d9abcaf3eba3
Ruben Arutyunyan <yordanov.ruben@gmail.com>
2026-06-01 21:25:11 +0300

    Create 08. Basketball Equipment.py

---
+++ First Steps in Coding - Exercise/08. Basketball Equipment.py
@@ -0,0 +1,10 @@
+budget = float(input())
+
+sneakers = budget - budget * 0.4
+drei = sneakers - sneakers * 0.2
+balls = drei * 0.25
+accessories = balls * 0.20
+
+sum = sneakers + drei + balls + accessories + budget
+
+print(sum)

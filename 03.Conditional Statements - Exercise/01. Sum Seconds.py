c0c8348b0f081cb6cf078ed5ba3339f3ef55bfcd
Ruben Arutyunyan <yordanov.ruben@gmail.com>
2026-06-02 10:34:56 +0300

    Create 01. Sum Seconds.py

---
+++ 03.Conditional Statements - Exercise/01. Sum Seconds.py
@@ -0,0 +1,14 @@
+import math
+
+first_time = int(input())
+second_time = int(input())
+third_time = int(input())
+
+total_time = first_time + second_time + third_time
+
+hours = total_time / 60
+hours = math.trunc(hours)
+minutes = total_time % 60
+
+print(f'{hours}:{minutes:02d}')
+# print(minutes)

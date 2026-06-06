d83fd45c8b6d2567d02099f7e38952a24c438492
Ruben Arutyunyan <yordanov.ruben@gmail.com>
2026-06-06 13:15:57 +0300

    Update 05. Travelling.py

--- 10.Nested Loops - Lab/05. Travelling.py
+++ 10.Nested Loops - Lab/05. Travelling.py
@@ -1,4 +1,18 @@
+total_sum = 0
+
+
 while True:
     destination =input()
+    if destination == 'End':
+        exit()
     budget = float(input())
-    
+
+
+    while True:
+        money = float(input())
+        total_sum += money
+        if total_sum >= budget:
+            print(f'Going to {destination}!')
+            total_sum = 0
+            budget = 0
+            break

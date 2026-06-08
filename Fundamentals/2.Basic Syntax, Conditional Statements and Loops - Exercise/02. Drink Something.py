4d052d1f83007deee58aca52a1d8748f413ef31b
Ruben Arutyunyan <yordanov.ruben@gmail.com>
2026-06-08 15:02:23 +0300

    exercise added

---
+++ Fundamentals/2.Basic Syntax, Conditional Statements and Loops - Exercise/02. Drink Something.py
@@ -0,0 +1,10 @@
+n = int(input())
+
+if 0 <= n <= 14:
+    print("drink toddy")
+elif 14 < n <= 18:
+    print("drink coke")
+elif 18 < n <= 21:
+    print("drink beer")
+else:
+    print("drink whisky")

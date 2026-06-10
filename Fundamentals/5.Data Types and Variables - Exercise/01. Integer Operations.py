96f3a043738f0ceea5fcdb5bc14d997aad7ae26a
Ruben Arutyunyan <yordanov.ruben@gmail.com>
2026-06-10 16:17:36 +0300

    Create 01. Integer Operations.py

---
+++ Fundamentals/5.Data Types and Variables - Exercise/01. Integer Operations.py
@@ -0,0 +1,10 @@
+first_number = int(input())
+second_number = int(input())
+third_number = int(input())
+fourth_number = int(input())
+
+first_number += second_number
+first_number /= third_number
+first_number = int(first_number) * fourth_number
+
+print(first_number)

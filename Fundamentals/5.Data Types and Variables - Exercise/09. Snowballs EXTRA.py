04c5688e69d71ebc42860e9c53b5a1a20a55217c
Ruben Arutyunyan <yordanov.ruben@gmail.com>
2026-06-11 11:31:10 +0300

    Create 09. Snowballs EXTRA.py

---
+++ Fundamentals/5.Data Types and Variables - Exercise/09. Snowballs EXTRA.py
@@ -0,0 +1,21 @@
+n = int(input())
+
+best_value = 0
+best_weight = 0
+best_time = 0
+best_quality = 0
+
+for _ in range(n):
+    weight = int(input())
+    time = int(input())
+    quality = int(input())
+
+    value = (weight // time) ** quality
+
+    if value > best_value:
+        best_value = value
+        best_weight = weight
+        best_time = time
+        best_quality = quality
+
+print(f"{best_weight} : {best_time} = {best_value} ({best_quality})")

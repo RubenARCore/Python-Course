a83fe70734dc5add456a2a687a438b9bd34784c6
Ruben Arutyunyan <yordanov.ruben@gmail.com>
2026-06-12 21:35:26 +0300

    Update 06. Survival of the Biggest.py

--- Fundamentals/8.Lists Basics - Exercise/06. Survival of the Biggest.py
+++ Fundamentals/8.Lists Basics - Exercise/06. Survival of the Biggest.py
@@ -1,15 +1,21 @@
 input_list = list(map(int, input().split(" ")))
 n = int(input())
-control_list = input_list[:]
-removed_list = []
-
-input_list.sort()
-
+# control_list = input_list[:]
+# removed_list = []
+#
+# input_list.sort()
+#
+#
+# for i in range(n):
+#     removed_list.append(input_list.pop(0))
+#
+# for i in range(len(removed_list)):
+#     control_list.remove(removed_list[i])
+#
+# print(", ".join(map(str, control_list)))
 
 for i in range(n):
-    removed_list.append(input_list.pop(0))
+    number = min(input_list)
+    input_list.remove(number)
 
-for i in range(len(removed_list)):
-    control_list.remove(removed_list[i])
-
-print(", ".join(map(str, control_list)))
+print(", ".join(map(str, input_list)))

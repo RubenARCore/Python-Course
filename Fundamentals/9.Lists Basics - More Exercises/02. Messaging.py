b27451860bf4e192da191fe761ff92024dd8b8b5
Ruben Arutyunyan <yordanov.ruben@gmail.com>
2026-06-15 10:38:04 +0300

    Update 02. Messaging.py

--- Fundamentals/9.Lists Basics - More Exercises/02. Messaging.py
+++ Fundamentals/9.Lists Basics - More Exercises/02. Messaging.py
@@ -1,5 +1,17 @@
 number = list(map(int, input().split()))
 chars = list(input())
+checker = False
 
+for i in range(len(number)):
+    control_number = number[i]
+    sum_digits = 0
 
+    while control_number > 0:
+        sum_digits += control_number % 10
+        control_number //= 10
 
+        if sum_digits > len(chars):
+            sum_digits %= len(chars)
+
+    print(f'{chars[sum_digits]}', end='')
+    chars.pop(sum_digits)

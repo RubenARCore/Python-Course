3005ae22f5dda9a06b20fb1dfa405f0ecd1ccaf0
Ruben Arutyunyan <yordanov.ruben@gmail.com>
2026-06-15 10:10:16 +0300

    Create 01. Zeros to Back.py

---
+++ Fundamentals/9.Lists Basics - More Exercises/01. Zeros to Back.py
@@ -0,0 +1,16 @@
+n = list(map(int, input().split(", ")))
+counter = 0
+
+
+# for i in range(len(n)):
+#     if 0 in n:
+#         n.remove(0)
+#         counter += 1
+#
+# for i in range(counter):
+#     n.append(0)
+#
+# print(n)
+
+while 0 in n:
+    n.remove(0)

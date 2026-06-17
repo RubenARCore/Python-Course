7f592b1bc01bd0a051e566f82aa6bb12b24df3b4
Ruben Arutyunyan <26250701+RubenARCore@users.noreply.github.com>
2026-06-17 17:02:22 +0300

    Create 08. Palindrome Integers.py

---
+++ Fundamentals/11.Functions - Exercise/08. Palindrome Integers.py
@@ -0,0 +1,8 @@
+n = list(map(int, input().split(", ")))
+
+def palindrome_integer(n_):
+
+    breakpoint()
+    for i in range(len(n_)):
+        if n_[i] == list(reversed(n_[i])):
+            pass

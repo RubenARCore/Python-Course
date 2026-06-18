b605e516edef41a9a1ba77a97755718ec35bea4f
Ruben Arutyunyan <26250701+RubenARCore@users.noreply.github.com>
2026-06-18 08:25:49 +0300

    Update 08. Palindrome Integers.py

--- Fundamentals/11.Functions - Exercise/08. Palindrome Integers.py
+++ Fundamentals/11.Functions - Exercise/08. Palindrome Integers.py
@@ -1,10 +1,25 @@
 n = input().split(", ")
 result_list = []
+
+
+def palindrome(num):
+    for i in range(0, len(num)):
+
+        reversed_n = list(reversed(num[i]))
+
+        if list(num[i]) == list(reversed_n):
+            result_list.append(True)
+        else:
+            result_list.append(False)
+
+    return result_list
+
+palindrome(n)
+
 for i in range(0, len(n)):
-
-    reversed_n = list(reversed(n[i]))
-
-    if list(n[i]) == list(reversed_n):
-        print("True")
+    if result_list[i]:
+        print('True')
     else:
-        print("False")
+        print('False')
+
+

2a39a2dc6587048645372c07c37e9d96bdc0fffc
Ruben Arutyunyan <26250701+RubenARCore@users.noreply.github.com>
2026-06-18 13:53:26 +0300

    Update 02. Center Point.py

--- Fundamentals/12.Functions - More Exercises/02. Center Point.py
+++ Fundamentals/12.Functions - More Exercises/02. Center Point.py
@@ -1,7 +1,17 @@
-result_list = []
+numbers = []
+for _ in range(4):
+    numbers.append(float(input()))
 
-for i in range(4):
-    result_list.append(float(input()))
-result_list.sort()
+def center_point(point):
+    x1, y1, x2, y2 = point
 
-print(", ".join(map(str, result_list[:2])))
+    d1 = x1 ** 2 + y1 ** 2
+    d2 = x2 ** 2 + y2 ** 2
+
+    if d1 <= d2:
+        return [int(x1), int(y1)]
+    else:
+        return [int(x2), int(y2)]
+
+result = center_point(numbers)
+print(f'({", ".join(map(str, result))})')

282b3447841ddfc82eaee6e638556d2b2c5a7ad3
Ruben Arutyunyan <26250701+RubenARCore@users.noreply.github.com>
2026-06-22 18:20:27 +0300

    Create 07. Group of 10's.py

---
+++ Fundamentals/14.Lists Advanced - Exercise/07. Group of 10's.py
@@ -0,0 +1,18 @@
+data = list(map(int, input().split(", ")))
+result = []
+start_point = 1
+
+if data[-1] % 10 == 0:
+    range_ = (data[-1] // 10) + 1
+else:
+    range_ = (data[-1] // 10) + 2
+
+for i in range(1, range_):
+
+    end_point = i * 10
+    current = [x for x in data if start_point <= x <= end_point]
+    start_point, end_point = end_point, start_point
+    result.append(current)
+
+for i in range(len(result)):
+    print(f"Group of {(i+1)*10}\'s: {result[i]}")

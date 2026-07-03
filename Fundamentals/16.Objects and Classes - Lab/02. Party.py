d71a0195e67da4e92fdd7613d265dff4bb5640dd
Ruben Arutyunyan <26250701+RubenARCore@users.noreply.github.com>
2026-07-03 11:35:06 +0300

    Create 02. Party.py

---
+++ Fundamentals/16.Objects and Classes - Lab/02. Party.py
@@ -0,0 +1,14 @@
+class Party:
+    def __init__(self):
+        self.people = []
+
+
+party = Party()
+
+data = input()
+while data != "End":
+    party.people.append(data)
+    data = input()
+
+print(f'Going: {", ".join(party.people)}')
+print(f'Total: {len(party.people)}')

a805c414d6e381b5f9b4f334bf234cbf858cf20e
Ruben Arutyunyan <26250701+RubenARCore@users.noreply.github.com>
2026-07-08 09:40:54 +0300

    Update 04. Students.py

--- Fundamentals/18.Dictionaries - Lab/04. Students.py
+++ Fundamentals/18.Dictionaries - Lab/04. Students.py
@@ -1,9 +1,24 @@
-data_lst = []
 students = {}
+course = None
+
+
 
 while True:
-    data = input().split(":", 2)
+    data = input().split(":")
     if len(data) == 1:
+        course = data[0].replace("_", " ")
         break
-    data_lst.append(data)
-    students[data[0]] = int(data[1])
+
+    name = data[0]
+    student_id = int(data[1])
+    course_name = data[2]
+
+    if course_name not in students:
+        students[course_name] = {}
+
+    students[course_name][name] = student_id
+
+for student in students[course]:
+    print(f"{student} - {students[course][student]}")
+
+

b9e6c49010f4c3bbd2ca691c8e09b378f8c491df
Ruben Arutyunyan <26250701+RubenARCore@users.noreply.github.com>
2026-07-08 21:44:04 +0300

    Update 07. SoftUni Parking.py

--- Fundamentals/19.Dictionaries - Exercise/07. SoftUni Parking.py
+++ Fundamentals/19.Dictionaries - Exercise/07. SoftUni Parking.py
@@ -4,13 +4,28 @@ registered = {}
 for i in range(n):
     data = input().split()
 
-    command, name, plate_number = data
+
+
+    if len(data) == 3:
+        command, name, plate_number = data
+    else:
+        command, name = data
+        plate_number = "registered[name]"
+
 
     if command == "register":
         if name not in registered:
             registered[name] = plate_number
-            print(f"{name} registered {plate_number}")
+            print(f"{name} registered {plate_number} successfully")
         else:
             print(f"ERROR: already registered with plate number {plate_number}")
     else:
-        registered.pop(name)
+        if data[1] not in registered:
+            print(f"ERROR: user {name} not found")
+            
+        else:
+            print(f"{name} unregistered successfully")
+            registered.pop(name)
+
+for name, plate_number in registered.items():
+    print(f"{name} => {plate_number}")

9b1a55e28f99d2b8f385ab1c0f449fc1ce07790c
Ruben Arutyunyan <26250701+RubenARCore@users.noreply.github.com>
2026-07-10 10:33:51 +0300

    Update 09. Student Academy.py

--- Fundamentals/19.Dictionaries - Exercise/09. Student Academy.py
+++ Fundamentals/19.Dictionaries - Exercise/09. Student Academy.py
@@ -1,5 +1,19 @@
 n = int(input())
 
-for _ in range(n):
+data_dict = {}
 
+for _ in range(n):
+    name = input()
+    grade = float(input())
+
+    if name not in data_dict:
+        data_dict[name] = [grade]
+    else:
+        data_dict[name].append(grade)
+
+for name, grades in data_dict.items():
+
+    total = sum(grades) / len(grades)
     
+    if total >= 4.5:
+        print(f"{name} -> {total:.2f}")

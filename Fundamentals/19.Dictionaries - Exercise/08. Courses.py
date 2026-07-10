a65d8b6c551dabf73eb22d25b1b909aaecceb915
Ruben Arutyunyan <26250701+RubenARCore@users.noreply.github.com>
2026-07-10 10:22:25 +0300

    Update 08. Courses.py

--- Fundamentals/19.Dictionaries - Exercise/08. Courses.py
+++ Fundamentals/19.Dictionaries - Exercise/08. Courses.py
@@ -3,5 +3,16 @@ courses = {}
 while True:
     data = input().split(" : ")
 
+    if data[0] == "end":
+        break
+
     if data[0] not in courses:
-        
+        courses[data[0]] = [data[1]]
+    else:
+        courses[data[0]].append(data[1])
+
+
+for name_of_course, course in courses.items():
+    print(f"{name_of_course}: {len(course)}")
+    for name in course:
+        print(f"-- {name}")

524f0dfcfab2ceeb526d005364971280646250fa
Ruben Arutyunyan <26250701+RubenARCore@users.noreply.github.com>
2026-07-11 14:58:14 +0300

    Update 01. Ranking.py

--- Fundamentals/20.Dictionaries - More Exercises/01. Ranking.py
+++ Fundamentals/20.Dictionaries - More Exercises/01. Ranking.py
@@ -3,24 +3,49 @@ submissions = {}
 
 while True:
     data = input()
-    if "end" in data:
+    if data == "end of contests":
         break
 
-    data = data.split(":")
-    contest = data[0]
-    password = data[1]
-
+    contest, password = data.split(":")
     contests[contest] = password
 
 while True:
     data = input()
-    if "end" in data:
+    if data == "end of submissions":
         break
 
-    data = data.split("=>")
-    contest = data[0]
-    password = data[1]
-    username = data[2]
-    points = data[3]
+    contest, password, username, points = data.split("=>")
+    points = int(points)
 
+    if contest in contests and contests[contest] == password:
 
+        if username not in submissions:
+            submissions[username] = {}
+
+        if contest not in submissions[username]:
+            submissions[username][contest] = points
+        elif points > submissions[username][contest]:
+            submissions[username][contest] = points
+
+best_candidate = max(
+    submissions,
+    key=lambda user: sum(submissions[user].values())
+)
+
+best_points = sum(submissions[best_candidate].values())
+
+print(
+    f"Best candidate is {best_candidate} "
+    f"with total {best_points} points."
+)
+
+print("Ranking:")
+
+for username in sorted(submissions):
+    print(username)
+
+    for contest, points in sorted(
+        submissions[username].items(),
+        key=lambda x: -x[1]
+    ):
+        print(f"#  {contest} -> {points}")

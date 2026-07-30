8e324896fad3b3c4ba68a57c5226638cbb1239e1
Ruben Arutyunyan <26250701+RubenARCore@users.noreply.github.com>
2026-07-30 17:00:20 +0300

    Update 01. World Tour.py

--- Fundamentals/27.Fundamentals - Exams/02. Programming Fundamentals Final Exam/01. World Tour.py
+++ Fundamentals/27.Fundamentals - Exams/02. Programming Fundamentals Final Exam/01. World Tour.py
@@ -2,7 +2,7 @@ data = list(input())
 
 while True:
     command = input().split(":")
-    if command == "Travel":
+    if command[0] == "Travel":
         break
 
     if command[0] == "Add Stop":
@@ -12,7 +12,6 @@ while True:
         if 0 <= index < len(data):
             for i in range(len(data_string)):
                 data.insert(index + i, data_string[i])
-            print("".join(data))
 
     elif command[0] == "Remove Stop":
         start_index = int(command[1])
@@ -21,14 +20,15 @@ while True:
         if 0 <= start_index < len(data) and 0 <= end_index < len(data):
             for j in range( (end_index - start_index) +1 ):
                 data.pop(start_index)
-            print("".join(data))
 
     elif command[0] == "Switch":
         old_string = command[1]
         new_String = command[2]
         control_string = "".join(data)
-        control_string.replace(old_string, new_String)
-        print(control_string)
+        control_string = control_string.replace(old_string, new_String)
+        data = list(control_string)
 
+    print("".join(data))
 
+print(f"Ready for world tour! Planned stops: {''.join(data)}")
 

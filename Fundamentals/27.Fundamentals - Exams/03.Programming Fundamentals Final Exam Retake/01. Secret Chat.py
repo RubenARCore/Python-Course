8f7f9572757ea5773393a82c5caaf14501426307
Ruben Arutyunyan <26250701+RubenARCore@users.noreply.github.com>
2026-07-31 17:08:45 +0300

    Update 01. Secret Chat.py

--- Fundamentals/27.Fundamentals - Exams/03.Programming Fundamentals Final Exam Retake/01. Secret Chat.py
+++ Fundamentals/27.Fundamentals - Exams/03.Programming Fundamentals Final Exam Retake/01. Secret Chat.py
@@ -9,15 +9,25 @@ while True:
 
     if command[0] == "InsertSpace":
         data.insert(int(command[1]), " ")
+        print("".join(data))
 
     elif command[0] == "Reverse":
         substring = command[1]
         control_value = "".join(data)
 
-        if substring in data:
+        if substring in control_value:
             control_value = control_value.replace(substring, "", 1) + substring[::-1]
-            data = list(wedded)
+            print(control_value)
+            data = list(control_value)
         else:
             print("error")
 
     elif command[0] == "ChangeAll":
+        substring = command[1]
+        replacement = command[2]
+        control_value = "".join(data)
+        control_value = control_value.replace(substring,replacement)
+        print(control_value)
+        data = list(control_value)
+
+print(f"You have a new text message: {''.join(data)}")

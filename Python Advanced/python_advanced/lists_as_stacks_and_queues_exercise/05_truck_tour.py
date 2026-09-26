b4a74178fcf6b46e58a73af5cebdfa3a5f3cdd22
Ruben Arutyunyan <yordanov.ruben@gmail.com>
2026-09-26 16:08:21 +0300

    Create 05_truck_tour.py

---
+++ Python Advanced/python_advanced/lists_as_stacks_and_queues_exercise/05_truck_tour.py
@@ -0,0 +1,26 @@
+from collections import deque
+
+pumps = deque()
+n = int(input())
+tank = 0
+starting_point_counter = 0
+length_counter = 0
+
+for i in range(n):
+    data = list(map(int, input().split()))
+    pumps.append(data)
+
+while True:
+    tank += pumps[0][0]
+    if tank >= pumps[0][1]:
+        tank -= pumps[0][1]
+        length_counter += 1
+        if length_counter == len(pumps):
+            break
+        continue
+    else:
+        pumps.append(pumps.popleft())
+        starting_point_counter += 1
+        length_counter = 0
+        tank = 0
+print(starting_point_counter)
